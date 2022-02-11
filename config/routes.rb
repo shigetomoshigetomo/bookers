cffbe2c1a85acbb5835b620463bd61521508b27f
EC2 Default User <ec2-user@ip-172-31-33-12.ec2.internal>
2022-02-11 03:45:14 +0000

    [Add] create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'books' =>'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/:id' => 'books#edit'
+  get 'books/:id' => 'books#show',as:'book'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#edit',as:'book_edit'
+  delete 'books/:id' => 'books#destroy',as:'book_destroy'
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
